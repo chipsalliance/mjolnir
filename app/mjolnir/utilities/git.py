95c73bd3e14f656db62c1d8b642407fe9d1614dd
Collin MacDonald <cmacd@google.com>
2026-06-17 21:05:20 +0000

    [v2 refactor] formatting

--- app/mjolnir/utilities/git.py
+++ app/mjolnir/utilities/git.py
@@ -18,10 +18,18 @@ def setup_repository(repo_url: str, code_dir: str, ref: str, workspace_dir: str)
     else:
         run_command(["git", "reset", "--hard"], cwd=code_dir)
         run_command(["git", "clean", "-fdx"], cwd=code_dir)
-        run_command(["git", "submodule", "foreach", "--recursive", "git", "reset", "--hard"], cwd=code_dir)
-        run_command(["git", "submodule", "foreach", "--recursive", "git", "clean", "-fdx"], cwd=code_dir)
+        run_command(
+            ["git", "submodule", "foreach", "--recursive", "git", "reset", "--hard"],
+            cwd=code_dir,
+        )
+        run_command(
+            ["git", "submodule", "foreach", "--recursive", "git", "clean", "-fdx"],
+            cwd=code_dir,
+        )
 
     # Perform Git Checkout
     if ref:
         run_command(["git", "checkout", ref], cwd=code_dir)
-        run_command(["git", "submodule", "update", "--init", "--recursive"], cwd=code_dir)
+        run_command(
+            ["git", "submodule", "update", "--init", "--recursive"], cwd=code_dir
+        )
