8f4da1f9656e0477e8776d7c2ae0fe1b6f490ac0
Collin MacDonald <cmacd@google.com>
2026-07-20 20:43:12 +0000

    refactor(init): remove unused init files
    Signed-off-by: Collin MacDonald <cmacd@google.com>

--- app/mjolnir/executors/__init__.py
+++
@@ -1,8 +0,0 @@
-# Licensed under the Apache-2.0 license
-# SPDX-License-Identifier: Apache-2.0
-"""Executors package encapsulating CLI tool runner abstractions."""
-
-from executors.ast_grep import AstGrepRunner
-from executors.ctags import CtagsRunner
-
-__all__ = ["AstGrepRunner", "CtagsRunner"]
--- app/mjolnir/security/__init__.py
+++
@@ -1,7 +0,0 @@
-# Licensed under the Apache-2.0 license
-# SPDX-License-Identifier: Apache-2.0
-"""Security package for Mjolnir."""
-
-from security.path_sanitizer import resolve_workspace_path
-
-__all__ = ["resolve_workspace_path"]
